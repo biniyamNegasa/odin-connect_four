3338ea995d510d58442c94965fc0359419009978
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-10-06 15:41:29 +0300

    feat: initialize with buckets

---
+++ lib/connect_four.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# The popular game where you have to have four consecutive dots to win
+class ConnectFour
+  COLUMNS = 7
+  ROWS = 6
+  def initialize
+    @buckets = []
+    COLUMNS.times { @buckets << [] }
+  end
+end
