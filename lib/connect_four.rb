2bedc5af9f4ac4984ca5bfb8d5c74034e1f10002
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-10-07 11:24:24 +0300

    feat: implement #game_over?

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -15,4 +15,27 @@ class ConnectFour
   def inbound?(row, col)
     row.between?(0, ROWS - 1) && col.between?(0, COLUMNS - 1)
   end
+
+  def game_over?(row, col)
+    drx = [[[0, 1], [0, -1]], [[1, 0], [-1, 0]], [[1, 1], [-1, -1]], [[-1, 1], [1, -1]]]
+
+    drx.each do |path|
+      first, second = path
+      consecutives = count(row, col, first) + count(row, col, second) - 1
+      return true if consecutives >= 4
+    end
+    false
+  end
+
+  def count(row, col, path)
+    val = buckets[col][row]
+    count = 0
+    r, c = path
+    while inbound?(row, col) && buckets[col][row] == val
+      row += r
+      col += c
+      count += 1
+    end
+    count
+  end
 end
