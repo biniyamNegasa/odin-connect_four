8fc5aa3f713be3ad992c8ce7ed8ce07d4f9919fe
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-10-07 09:41:16 +0300

    test: Add tests for #inbound?

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,10 +3,25 @@
 require_relative '../lib/connect_four'
 
 describe ConnectFour do
+  subject(:connect_four) { described_class.new }
   describe '#initialize' do
-    subject(:connect_four) { described_class.new }
     it 'creates a bucket array with 7 columns' do
       expect(connect_four.buckets).to eq([[], [], [], [], [], [], []])
+    end
+  end
+
+  describe '#inbound?' do
+    it 'returns true if column is between 0 and 6 and row is between 0 and 5' do
+      expect(connect_four.inbound?(1, 4)).to be(true)
+    end
+    it 'returns false if column is between 0 and 6 but row is out of bound' do
+      expect(connect_four.inbound?(1, 6)).to be(false)
+    end
+    it 'returns false if column is out of bound and row is inbound' do
+      expect(connect_four.inbound?(8, 4)).to be(false)
+    end
+    it 'returns false if column is out of bound and row is also out of bound' do
+      expect(connect_four.inbound?(8, 10)).to be(false)
     end
   end
 end
